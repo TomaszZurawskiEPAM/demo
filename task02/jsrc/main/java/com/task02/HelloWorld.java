bf93b12ded114db535aa91ef2311dd2d44f6c204
TomaszZurawskiEPAM <tomasz_zurawski@epam.com>
2024-07-05 15:21:37 +0200

    task02- again

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -20,8 +20,7 @@ import java.util.Map;
 
 @LambdaHandler(lambdaName = "hello_world",
 				roleName = "hello_world-role",
-				isPublishVersion = true,
-				aliasName = "${lambdas_alias_name}",
+				isPublishVersion = false,
 				logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @LambdaUrlConfig(
