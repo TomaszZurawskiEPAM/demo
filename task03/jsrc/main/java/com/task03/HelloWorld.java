1fd5fcd3fbf039ef716ead5c15c424311f1b9aa2
TomaszZurawskiEPAM <tomasz_zurawski@epam.com>
2024-07-24 16:50:21 +0200

    task03-1

--- task03/jsrc/main/java/com/task03/HelloWorld.java
+++ task03/jsrc/main/java/com/task03/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.task01;
+package com.task03;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -8,8 +8,7 @@ import com.syndicate.deployment.model.RetentionSetting;
 import java.util.HashMap;
 import java.util.Map;
 
-@LambdaHandler(lambdaName = "hello_world",
-	roleName = "hello_world-role",
+@LambdaHandler(lambdaName = "hello_world",roleName = "hello_world-role",
 	isPublishVersion = true,
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
@@ -19,8 +18,8 @@ public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 	public Map<String, Object> handleRequest(Object request, Context context) {
 		System.out.println("Hello from lambda");
 		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("message", "Hello from Lambda");
 		resultMap.put("statusCode", 200);
+		resultMap.put("message", "Hello from Lambda");
 		return resultMap;
 	}
 }
