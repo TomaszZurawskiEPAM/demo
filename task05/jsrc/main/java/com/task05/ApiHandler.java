74e56dc276953d12867b7fb7a7df0671ac9067fc
TomaszZurawskiEPAM <tomasz_zurawski@epam.com>
2024-08-02 15:41:15 +0200

    task05 attempt 2

--- task05/jsrc/main/java/com/task05/ApiHandler.java
+++ task05/jsrc/main/java/com/task05/ApiHandler.java
@@ -18,8 +18,9 @@ public class ApiHandler implements RequestHandler<Object, Map<String, Object>> {
 	public Map<String, Object> handleRequest(Object request, Context context) {
 		System.out.println("Hello from lambda");
 		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
+		resultMap.put("statusCode", 201);
+		resultMap.put("event", "Hello from Lambda");
+
 		return resultMap;
 	}
 }
